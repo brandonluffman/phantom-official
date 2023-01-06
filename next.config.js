fe7133442d9ca4182f19b72e2baeea9734e3c48c
Brandon Luffman <brandonluff10@gmail.com>
2023-01-06 13:21:40 -0500

    Next config change.

--- next.config.js
+++ next.config.js
@@ -12,12 +12,5 @@ const nextConfig = {
   }
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-});
 
 module.exports = nextConfig;
